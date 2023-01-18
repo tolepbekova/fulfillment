119e25f6dd9938fc45d1d129ed5a534486da0fa9
Nurbolat Abdraim <nurlight05@gmail.com>
2023-01-18 18:55:42 +0600

    Сделал отправку excel

--- src/views/ViewInvoice.vue
+++ src/views/ViewInvoice.vue
@@ -1,11 +1,15 @@
 <template>
     <div class="view-invoice">
         <div class="view-invoice__inner">
-            <container>
-                <form action="">
+            <v-container>
+                <form @submit.prevent="sendFile()" action="">
                     <v-row class="mt-5">
                         <v-col>
                             <v-file-input
+                                
+                                id="file"
+                                ref="file"
+                                v-on:change="handleFileUpload"
                                 accept=".xlsx"
                                 label="Загрузить Excel"
                             ></v-file-input>
@@ -14,6 +18,7 @@
                             <v-btn
                             color="warning"
                             dark
+                            type="submit"
                             >
                                 Загрузить
                             </v-btn>
@@ -21,7 +26,39 @@
                         <v-col></v-col>
                     </v-row>
                 </form>
-            </container>
+            </v-container>
         </div>
     </div>
 </template>
+
+<script>
+import axios from 'axios'
+export default {
+    data: () => ({
+        file: ''
+    }),
+    methods:{
+        sendFile(){
+            // let docForm = document.getElementById('docForm')
+            // let formData = new FormData(docForm);
+            // console.log(formData)
+            let formData = new FormData();
+            formData.append('file', this.file);
+            axios.post('http://87.255.194.27:8001/api/goods/download/',
+            
+                formData,
+            
+            {
+                headers:{
+                    Authorization: 'Token ' + localStorage.getItem('usertoken'),
+                    'Content-Type': 'multipart/form-data'
+                }
+            })
+        },
+        handleFileUpload: function(file){
+            this.file = file;
+            console.log(this.file)
+        }
+    }
+}
+</script>
